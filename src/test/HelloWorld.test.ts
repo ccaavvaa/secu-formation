9de0ef4fe3bd66d0cdeaa73e521aa587d8f028ae
Catalin VASCU <c.vascu@salviadeveloppement.com>
2025-09-26 13:20:06 +0200

    remove old test file

--- src/test/HelloWorld.test.ts
+++
@@ -1,8 +0,0 @@
-import assert from 'node:assert';
-import test from 'node:test';
-import { HelloWorld } from '../lib/HelloWorld.js';
-
-test('HelloWorld', () => {
-  const helloWorld = new HelloWorld();
-  assert.strictEqual(helloWorld.sayHello(), 'Hello, world!');
-});
